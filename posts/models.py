de25fc9cef8b0b2d2a9fed7e2b880cfda3cfe83b
Kulasq <lucasdantas_11@hotmail.com>
2025-10-21 15:06:48 -0300

    create 'like' model

--- posts/models.py
+++ posts/models.py
@@ -29,3 +29,14 @@ class Post(models.Model):
     @property
     def comments_count(self):
         return self.comments.count()
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ['user', 'post']
+    
+    def __str__(self):
+        return f"{self.user.username} curtiu {self.post.id}"
